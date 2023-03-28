673a5fa029df787aa5c8c7188a34727f87159177
Krish <krishparekh9@gmail.com>
2023-03-28 15:53:16 +0530

    Changes to build

--- src/components/EventLink.tsx
+++ src/components/EventLink.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export const EventLinkComponent = () => {
     return (
       <div style={{width: "100%", height: "100%", display: "flex", justifyContent: "center", alignItems: "center"}}>
